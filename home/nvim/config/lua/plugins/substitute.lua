d157be45f56c0806ccaad206273c81c33c7d2f1c
Aaron Xheres Tan <aaron.xheres@gmail.com>
2024-10-15 01:46:46 +0800

    update nvim shortcut for subtitude

--- home/nvim/config/lua/plugins/substitute.lua
+++ home/nvim/config/lua/plugins/substitute.lua
@@ -9,8 +9,8 @@ return {
     local keymap = vim.keymap
 
     vim.keymap.set("n", "s", substitute.operator, { desc = "Substitute: Motion" })
-    vim.keymap.set("n", "ss", substitute.line, { desc = "Substitute: Line" })
+    vim.keymap.set("n", "s$", substitute.line, { desc = "Substitute: Line" })
     vim.keymap.set("n", "S", substitute.eol, { desc = "Substitute: End of line" })
     vim.keymap.set("x", "s", substitute.visual, { desc = "Substitute: Visual Mode" })
-  end
+  end,
 }
