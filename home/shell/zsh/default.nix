8a197eb218b56e5820438c312b9fad02a726a9e1
Aaron Xheres Tan <aaron.xheres@gmail.com>
2024-10-14 23:19:58 +0800

    added preview for fzf | added alias for ls

--- home/shell/zsh/default.nix
+++ home/shell/zsh/default.nix
@@ -24,6 +24,8 @@
       export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git"
       export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
       export FZF_ALT_C_COMMAND="fd --type=d --hidden --strip-cwd-prefix --exclude .git"
+      export FZF_CTRL_T_OPTS="--preview 'bat -n --color=always --line-range :500 {}'"
+      export FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {} | head -200'"
 
       _fzf_compgen_path() {
         fd --hidden --exclude .git . "$1"
@@ -31,6 +33,18 @@
 
       _fzf_compgen_dir() {
         fd --type=d --hidden --exclude .git . "$1"
+      }
+
+      _fzf_comprun() {
+        local command=$1
+        shift
+
+        case "$command" in
+          cd)             fzf --preview 'eza --tree --color=always {} | head -200' "$@" ;;
+          export|unset)   fzf --preview "eval 'echo \$' {}" "$@"  ;;
+          ssh)            fzf --preview "dig {}" "$@" ;;
+          *)              fzf --preview "--preview 'bat -n --color=always --line-range :500 {}'" "$@" ;;
+        esac
       }
 
       # -- Sources
@@ -51,6 +65,7 @@
       vimdiff = "nvim -d";
 
       # ls
+      ls = "eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions";
       lla = "eza -llah --group-directories-first";
 
       # cat
