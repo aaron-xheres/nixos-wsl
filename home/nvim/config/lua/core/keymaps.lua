98e8ae2a827d9ae56e2bdb81bc2d9f0c3da10fdb
Aaron Xheres Tan <aaron.xheres@gmail.com>
2024-10-19 23:33:21 +0800

    nvim added terminal keymaps

--- home/nvim/config/lua/core/keymaps.lua
+++ home/nvim/config/lua/core/keymaps.lua
@@ -21,3 +21,10 @@ keymap.set("n", "<leader>tc", "<cmd>tabclose<cr>", { desc = "Tab: Close" })
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<cr>", { desc = "Tab: New current buffer" })
 keymap.set("n", "<C-]>", "<cmd>tabn<cr>", { desc = "Tab: Next" })
 keymap.set("n", "<C-[>", "<cmd>tabp<cr>", { desc = "Tab: Previous" })
+
+-- Terminal Management
+keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Terminal: Escape" })
+keymap.set("t", "<C-k>", "<C-\\><C-n><C-w>k", { desc = "Terminal: Focus Up" })
+keymap.set("t", "<C-j>", "<C-\\><C-n><C-w>j", { desc = "Terminal: Focus Down" })
+keymap.set("t", "<C-h>", "<C-\\><C-n><C-w>h", { desc = "Terminal: Focus Left" })
+keymap.set("t", "<C-l>", "<C-\\><C-n><C-w>l", { desc = "Terminal: Focus Right" })
