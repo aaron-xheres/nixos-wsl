24cf52610ba6d18d3ad1b4d20cc5996997656567
Aaron Xheres Tan <aaron.xheres@gmail.com>
2024-10-19 18:39:01 +0800

    nvim LSP added clippy as rust linter

--- home/nvim/config/lua/plugins/linting.lua
+++ home/nvim/config/lua/plugins/linting.lua
@@ -10,6 +10,7 @@ return {
       javascriptreact = { "eslint_d" },
       typescriptreact = { "eslint_d" },
       python = { "pylint" },
+      rust = { "clippy" },
     }
 
     -- eslint_d to ignore no config error
