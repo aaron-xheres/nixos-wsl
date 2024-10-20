27e6a880df075011843a8e2544245067c11c7ab5
Aaron Xheres Tan <aaron.xheres@gmail.com>
2024-10-20 16:23:52 +0800

    nvim configuration to be sources seperately
    This is to prevent lazy-lock from triggering error whenever it tries to
    install or update plugins due to the directory being read only

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -1,6 +1,14 @@
-{ pkgs-unstable, ... }: {
-  home.file.".config/nvim" = {
-    source = ./config;
+{ pkgs-unstable, config, ... }: {
+  # Split configs into individual directories to prevent symlinking the entire directory
+  # This will allow lazy-lock to be created and not get error messages
+  home.file.".config/nvim/init.lua".source = config.lib.file.mkOutOfStoreSymlink ./config/init.lua;
+  home.file.".config/nvim/lua/lazyvim.lua".source = config.lib.file.mkOutOfStoreSymlink ./config/lua/lazyvim.lua;
+  home.file.".config/nvim/lua/core" = {
+    source = config.lib.file.mkOutOfStoreSymlink ./config/lua/core;
+    recursive = true;
+  };
+  home.file.".config/nvim/lua/plugins" = {
+    source =  config.lib.file.mkOutOfStoreSymlink ./config/lua/plugins;
     recursive = true;
   };
 
@@ -13,4 +21,7 @@
     vimAlias = true;
     vimdiffAlias = true;
   };
+
+  # Formatter Config
+  home.file.".config/rustfmt/rustfmt.toml".source = config.lib.file.mkOutOfStoreSymlink ./fmt-config/rustfmt.toml;
 }
